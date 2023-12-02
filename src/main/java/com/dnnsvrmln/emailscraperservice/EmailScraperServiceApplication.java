1b9ef203d6b6290d70a1fc884c76ea1a40182a0d
dnnsvrmln <drw.vermeulen@gmail.com>
2023-12-02 14:41:48 +0100

    ESS-1. Basic project setup
    - Created a basic Java Spring Boot application with Spring Initialzr

---
+++ src/main/java/com/dnnsvrmln/emailscraperservice/EmailScraperServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dnnsvrmln.emailscraperservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailScraperServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailScraperServiceApplication.class, args);
+	}
+
+}
